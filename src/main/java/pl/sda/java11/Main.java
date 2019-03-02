6f7c998eb81f1889aaff73c5428acca2cf1ed64b
kamisamapl <jagla1990@gmail.com>
2019-03-02 14:50:56 +0100

    Dodawanie do repozytorium1

--- src/main/java/pl/sda/java11/Main.java
+++ src/main/java/pl/sda/java11/Main.java
@@ -1,8 +1,25 @@
 package pl.sda.java11;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        var zmienna = "to jest wartosc";
+        var list = new ArrayList<String>();
+        list.add("czesc");
+
+        System.out.println(zmienna);
+        System.out.println(list);
+
+        var l = List.of("A","B","C");
+        var copy = List.copyOf(l);
+
+        var map = Map.of("A",1,"B",2);
+        System.out.println(map);
     }
 }
